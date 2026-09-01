d71e9111901f4727aa9afed683e6dde297b97bae
molten-goldenchrome <edhannah@protonmail.com>
2026-09-01 16:18:10 +0100

    Added classes
    Adddedd the main classes and adapted the variables from original telium.py to fit within the classes. ignore the typos in this commit

--- Telium (second version)/telium.py
+++ Telium (second version)/telium.py
@@ -1,40 +1,63 @@
 import time; import sys; import random
 
-module = 1
-lastModule = 0
-possibleMoves = []
-alive = True
-won = False
-power = 100
-fuel = 500
-locked = 0
-queen = 0
-ventShafts = []
-infoPanels = []
-workers = []
+class Module:
+    currentModule = 1
+    lastModule = 0
+    def __init__(self):
+        pass
 
-class Module():
+    def loadModule(self):
+        global module
+        possibleMoves,roomInfo = station.getModuleInfo(module,station.map)
+        station.outputModule(module,roomInfo); station.outputMoves(possibleMoves)
+
+    def getCurrentModule(self):
+        return self.currentModule
+
+    def getLastModule(self):
+        return self.lastModule
+
+    def getPossibleMoves(self):
+        return station.getModuleInfo(module,station.map)[0]
+
+
+
+class Player:
+    alive = True
+    won = False
+    def __init__(self):
+        pass
+
+    def getAction(self):
+        localModule = module.getCurrentModule()
+        localLastModule = module.getLastModule()
+        localPossibleMoves = module.getPossibleMoves()
+        validAction = False
+        while validAction == False:
+            print("What do you want to do next? (MOVE, SCANNER)")
+            action = input(">>>")
+            result = textParser(action)
+
+class Telium:
+    queen = 0
+
+class InfoPanel:
+    infoPanels = []
+
+class Worker:
+    workers = []
+
+class Scanner:
     pass
 
-class Player():
-    pass
+class Vent:
+    ventShafts = []
 
-class Telium():
-    pass
-
-class InfoPanel():
-    pass
-
-class Worker():
-    pass
-
-class Scanner():
-    pass
-
-class Vent():
-    pass
-
-class Station():
+class Station:
+    map = "Charles_Darwin"
+    power = 100
+    fuel = 500
+    locked = 0
     def __init__(self,map):
         #loads the nuumber of modules, the map
         global numModules, gameMap
@@ -52,8 +75,77 @@ class Station():
         gameMap = gameMapTemp.read()
         gameMapTemp.close()
 
-    def getModuleInfo(module,map):
+    def getModuleInfo(self,module,map):
         #retrieves information about a specific module
         moves = []
         temp = open(f"resources/{map}/module{module}.txt","r")
-        
+        for i in range(0,4):
+            moveRead = temp.readline()
+            moveRead = int(moveRead.strip())
+            if moveRead != 0:
+                moves.append(moveRead)
+        roomInfo = temp.readline()
+        temp.close()
+        return moves,roomInfo
+
+    def outputModule(self,module,roomInfo):
+        print(f"\n-------------------------------------------------\nYou are in module {module}.\n{roomInfo}")
+
+    def outputMoves(self,possibleMoves):
+        print(f"From here, you can move to modules: | ",end='')
+        for move in possibleMoves:
+            print(move,'| ',end='')
+        print()
+
+def textParser(command):
+    global power, module, infoPanels
+    command = command.lower()
+    command = command.strip(' ')
+    command = command.strip(".")
+    action = None
+    detail = None
+    if 'move' in command or ('m' in command and 'map' not in command):
+        action = 'move'
+        command = command.strip("move")
+        try:
+            detail = int(command)
+            return (action,detail)
+        except:
+            return (False,False)
+    elif 'map' in command:
+        action = 'map'
+        return (action,detail)
+    elif 'look' in command:
+        action = 'look'
+        return (action,detail)
+    elif 'scanner' in command:
+        return ("scanner",None)
+    elif 'lock' in command or ('l' in command and 'lock' not in command):
+        action = 'lock'
+        command = command.strip("lock")
+        try:
+            detail = int(command)
+            return (action,detail)
+        except:
+            return (False,False)
+    elif 'panel' in command:
+        if power >= 51 and module in infoPanels:
+            return ('panel',"allowed")
+        else:
+            return ('panel','not allowed')
+    else:
+        return (False,False)
+
+
+
+
+
+
+module = Module()
+station = Station(map="Charles_Darwin") #change this so custom ones can be made
+scanner = Scanner()
+vents = Vent()
+workers = Worker()
+player = Player()
+panels = InfoPanel()
+queen = Telium()
